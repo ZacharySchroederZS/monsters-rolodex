27a3b739dc32ebdbf1405424341b9a32b795eaf3
Zachary Schroeder <zacharyschroederzs@gmail.com>
2020-01-18 20:46:45 -0600

    Updates

---
+++ precache-manifest.934239e9d90d785a673e8b65fe7071db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f12e0e1f49a5316db30b02957336eb7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1b989a25483cbecae1a6",
+    "url": "/monsters-rolodex/static/css/main.31639b53.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b989a25483cbecae1a6",
+    "url": "/monsters-rolodex/static/js/main.05631c81.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
